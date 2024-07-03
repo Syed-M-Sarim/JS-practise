832cb4d1ad8841b217190513fc17cecc3cbe021a
Syed Muhammad Sarim <iamsarim1000@gmail.com>
2024-07-03 21:26:27 +0500

    practise

--- app.js
+++ app.js
@@ -1,15 +1,69 @@
-var maths = +prompt("Your maths marks")
+// var maths = +prompt("Your maths marks")
 
-var eng = +prompt("Your eng marks")
+// var eng = +prompt("Your eng marks")
 
-var urdu = +prompt("Your urdu marks")
+// var urdu = +prompt("Your urdu marks")
 
-console.log(typeof maths)
+// console.log(typeof maths)
 
-console.log(typeof urdu)
+// console.log(typeof urdu)
 
-console.log(typeof eng)
+// console.log(typeof eng)
 
-var result = (maths + eng + urdu) /300*100
+// var result = (maths + eng + urdu) /300*100
 
-document.write("Your percentage is " + result + "%")
+// document.write("Your percentage is " + result + "%")
+
+// let size = "S"
+
+// if(size === "XL"){
+//     console.log("Price is 400rs")
+// }else if(size === "L"){
+//     console.log("Price is 400rs")
+// }else if(size === "M"){
+//     console.log("Price is 300rs")
+// }else{
+//     console.log("Price is 200rs")
+// }
+
+// let str = "good string"
+
+// if(str[0]==="a" && str.length > 3){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// let day = 9
+
+// switch(day){
+//     case 1:
+//         console.log("Monday")
+// break;
+// case 2:
+//     console.log("Tuesday")
+// break;
+// case 3:
+//     console.log("Wedday")
+// break;
+// case 4:
+// console.log("Thursday")
+// break;
+// case 5:
+//     console.log("Friday")
+// break;
+// case 6:
+// console.log("Saturday")
+// break;
+// case 7:
+//     console.log("Sunday")
+// break;
+// default:
+//     console.log("Not Confirmed")
+// }
+
+// let age = +prompt("Enter age");
+// let name = prompt("Enter name")
+
+// console.log(`${name} is ${age} years old`)
